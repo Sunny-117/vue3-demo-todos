3dc809a9d175af3cd2faa75247ecc44f18255a1c
Sunny-117 <73089592+Sunny-117@users.noreply.github.com>
2021-10-04 19:01:47 +0800

    Add files via upload

---
+++ src/composition/useEditTodo.js
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue'
+export default function useEditTodo(todosRef) {
+    const editingTodoRef = ref(null);//正在修改的todo
+    let originTitle = null;
+    const editTodo = (todo) => {
+        originTitle = todo.title;
+        editingTodoRef.value = todo;
+    }
+    const doneEdit = () => {
+        editingTodoRef.value = null;
+    }
+    const cancelEdit = (todo) => {
+        editingTodoRef.value = null;
+        // 把这个todo恢复到之前的样子
+        // todo.title = 之前的值
+        // 之前的值从哪来？提前保存一个变量
+        todo.title = originTitle;
+    }
+    // const setAllCompleted = (completed) => {
+    //     todosRef.value.forEach((todo) => {
+    //         todo.completed = completed;
+    //     })
+    // }
+    const allDoneRef = computed({
+        get() {
+            //全部完成就是true,反之false
+            return todosRef.value.filter(it => !it.completed).length === 0;//所有未完成任务的数量是不是等于0
+        },
+        set(checked) {
+            todosRef.value.forEach(todo => {
+                todo.completed = checked;
+            });
+        }
+    })
+    return {
+        editingTodoRef,
+        editTodo,
+        doneEdit,
+        cancelEdit,
+        allDoneRef
+    }
+
+}
